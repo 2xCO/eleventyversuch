e57402edfb620507ec1d836c13c1df13cdb2d0de
Corinna_K <corinna.kunst@icloud.com>
2026-04-01 18:41:46 +0200

    neu

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+    
+    eleventyConfig.addPassthroughCopy("js");
+    // eleventyConfig.addPassthroughCopy("**/*.css");
+    eleventyConfig.addPassthroughCopy("**/*.woff2");
+    eleventyConfig.addPassthroughCopy("**/*.zip");
+    eleventyConfig.addPassthroughCopy("**/*.jpg");
+    eleventyConfig.addPassthroughCopy("**/*.png");
+    eleventyConfig.addPassthroughCopy("assets/css");
+    eleventyConfig.addPassthroughCopy("assets/img");
+
+    
+};
